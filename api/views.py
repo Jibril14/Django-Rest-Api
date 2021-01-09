25f7d6d9b280dee127e5a0613dbd47bcf2a7e0ed
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2021-01-09 09:24:09 +0100

    Create Post via Api

--- api/views.py
+++ api/views.py
@@ -5,6 +5,6 @@ from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
 
-class postlist(generics.ListAPIView):
+class postlist(generics.ListCreateAPIView):
    queryset = Post.objects.all()
    serializer_class = PostSerializer
