c769117e59e140fd36e1d4e7e2202f8152b12b22
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2021-01-10 10:58:12 +0100

    DownVoting Post

--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, mixins, status
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 from .models import Post, Vote
 from .serializers import PostSerializer, VoteSerializer
 
@@ -12,7 +14,7 @@ class postlist(generics.ListCreateAPIView):
    def perform_create(self, serializer):
       serializer.save(poster=self.request.user)
 
-class VoteCreate(generics.CreateAPIView):
+class VoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
    serializer_class = VoteSerializer
    permission_classes = [permissions.IsAuthenticated]
 
@@ -25,3 +27,11 @@ class VoteCreate(generics.CreateAPIView):
       if self.get_queryset().exists():
          raise ValidationError("You Voted already")
       serializer.save(voter = self.request.user, post=Post.objects.get(pk=self.kwargs["pk"]))
+
+
+   def delete(self, request, *args, **kwargs):
+      if self.get_queryset().exists():
+         self.get_queryset().delete()
+         return Response(status=status.HTTP_204_NO_CONTENT)
+      else:
+         raise ValidationError("You never voter for this post!")
