680ff6bc30bb01688a7d98baf12bb9d7d946eb1c
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2021-01-10 19:55:07 +0100

    Add a Custom Field

--- api/serializers.py
+++ api/serializers.py
@@ -3,10 +3,15 @@ from .models import Post, Vote
 
 class PostSerializer(serializers.ModelSerializer):
     poster = serializers.ReadOnlyField(source = "poster.username")
+    all_votes = serializers.SerializerMethodField()
+
     class Meta:
         model = Post
-        fields =  ["id", "title", "url", "poster", "createdon" ]
+        fields =  ["id", "title", "url", "poster", "createdon", "all_votes"]
 
+
+    def get_all_votes(self, obj):
+        return Vote.objects.filter(post=obj).count()
 
 class VoteSerializer(serializers.ModelSerializer):
     class Meta:
